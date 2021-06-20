8228d9260e68f2deb81cbbeecf1db93862052179
aishat001 <waheedaishat@gmail.com>
2021-06-20 12:23:09 +0100

    fix

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URL
+const url = process.env.MONGODB_URI
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 .then(result => {
     console.log('connected to MongoDB')
